4c12a434329a91f822ad6d97f58000db54977813
Micheal Leverton <levertonmicheal@gmail.com>
2022-12-28 02:45:54 -0500

    about started

--- src/About.js
+++ src/About.js
@@ -1,27 +1,15 @@
 import React from 'react'
+import ProfilePic from './assets/profilepic.png'
 
 function About() {
   return (
-    <section id='about'>
-    <div className='textwhite mt-5'><p>Cillum est incididunt quis nulla aliqua elit sint excepteur quis pariatur. Velit amet quis aute nostrud. Elit proident cillum dolor ex voluptate ad do. Do magna veniam mollit duis veniam esse. Velit minim est est quis non cupidatat ea cupidatat cillum et elit.
-
-Deserunt excepteur nisi exercitation nulla Lorem occaecat non ipsum irure. Ipsum reprehenderit anim sint enim aliqua eiusmod sit aute. Consectetur cillum proident incididunt quis voluptate nulla. Fugiat labore dolore tempor mollit minim ad enim mollit cillum consectetur eiusmod irure. Duis in est Lorem irure proident irure minim. Elit id et in qui eu ad amet tempor in enim aliqua deserunt.
-
-Eiusmod ad amet proident dolor veniam. Sint ut enim laborum aute. Ipsum magna nulla deserunt duis cillum irure cillum excepteur velit.
-
-Id laboris dolore laboris consequat irure. Irure consequat incididunt sint adipisicing. Labore elit nulla aliqua qui laboris laborum Lorem magna et aliqua nisi veniam sint deserunt. Velit cillum laboris amet proident aliqua ullamco commodo. Consectetur minim qui dolor ea ex.
-
-Ad do elit exercitation irure eiusmod exercitation. Sunt incididunt cillum duis cupidatat ut proident aliqua consequat qui consectetur ullamco velit aute. Cupidatat exercitation Lorem magna Lorem veniam consectetur adipisicing est veniam minim ut aliquip sunt. Esse ea ex laboris do voluptate consequat proident excepteur incididunt qui esse in.
-
-Et ex consequat ex ex est cillum. Amet sint excepteur laborum duis officia mollit. Minim cupidatat non fugiat ut laboris magna et deserunt eu voluptate. Lorem amet Lorem laboris enim et elit ullamco nulla dolor pariatur ad do ullamco eiusmod. Nulla tempor incididunt in amet nisi exercitation quis ullamco consequat.
-
-Aliqua non sunt enim tempor velit cupidatat velit ex id exercitation elit cupidatat consectetur. Consequat duis ea non ad magna aute laborum est occaecat sunt. Id ut reprehenderit exercitation reprehenderit laboris aute ullamco velit fugiat cupidatat commodo minim culpa.
-
-Exercitation irure ipsum ex eu deserunt Lorem eiusmod enim laboris excepteur dolore. Sit ut irure minim nostrud officia ad duis do commodo. Est cillum esse eiusmod in. Sint consequat veniam sit cillum.
-
-Qui fugiat consectetur laborum enim ex adipisicing aliqua nostrud aliquip aliqua. Dolor et deserunt esse est irure excepteur. Elit fugiat ex non excepteur adipisicing sint cupidatat. Veniam ullamco velit sit sunt in labore ipsum anim sunt reprehenderit aute. Sit velit laboris ipsum ut adipisicing eiusmod amet qui nulla cillum fugiat. Pariatur sit nostrud cillum pariatur consequat proident.
-
-Aliqua esse esse labore ipsum. Minim adipisicing ut magna cillum nisi occaecat tempor ipsum dolore labore. Anim consequat commodo aliquip enim do enim labore sint nostrud velit ad officia officia enim. Ullamco non in fugiat sint officia sint aute ea sint.</p></div>
+    <section className='' id='about'>
+    <div className='profilecontainer textwhite mt-5'>
+      <div className='profilename'>Micheal Leverton</div>
+      <div className='profiledescription'>Developer And Designer</div>
+      <div className='profileaboutme'>Not even a year after the first computer came to market  in 1971, I was born. 8 years later I got my first computer and learned basic language. I've been coding ever since. I have seen the progression of the Web from start to Web3.0 <br/>Let me help you and your team continue to succeed. </div>
+      <div className='profilepic'><img alt="" src={ProfilePic} /></div>
+    </div>
     </section>
   )
 }
