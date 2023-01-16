444014a017c606fe71a3d2dbdfaec4d8821464cf
Micheal Leverton <levertonmicheal@gmail.com>
2023-01-16 04:03:27 -0500

    add form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,36 +1,24 @@
 import React from 'react'
-import "./styles.css"
 
 function ContactForm() {
+
 	return (
 		<div className='contactcardholder'>
 
 			<div className='contactcard'>
 
-				<form action="https://gmail.us21.list-manage.com/subscribe/post?u=ac1c083751d9fe609a58f1cfd&amp;id=9bda47a092&amp;f_id=000bcde1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					<div>
-						<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-						</label>
-						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required />
+				<form action="https://gmail.us21.list-manage.com/subscribe/post?u=ac1c083751d9fe609a58f1cfd&amp;id=9bda47a092&amp;f_id=000bcde1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
+					<input name="MMERGE3" class="" id="mce-MMERGE3" type="text" placeholder="Name" require />
+					<input name="EMAIL" type="email" id="mce-EMAIL" placeholder="Email" require />
+					<input name="FSUBJECT" type="" id="mce-FSUBJECT" placeholder="Subject" />
+					<input name="PHONE" type="number" id="mce-PHONE" placeholder="Phone Number" />
+					<textarea name="MMERGE2" id="mce-MMERGE2" cols="20" rows="8" placeholder="Message..."></textarea>
+					<div class="optionalParent">
+						<div class="clear foot">
+							<input type="submit" value="Send" name="subscribe" id="mc-embedded-subscribe" class="button" />
+								<p class="brandingLogo collapse"><a href="http://eepurl.com/ih8RSr" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" alt="" /></a></p>
+						</div>
 					</div>
-					<div>
-						<label for="mce-FSUBJECT">Subject </label>
-						<input type="text" value="" name="FSUBJECT" class="" id="mce-FSUBJECT" />
-					</div>
-					<div>
-						<label for="mce-MMERGE6">Starting Salary Offered </label>
-						<input type="number" name="MMERGE6" class="" value="" id="mce-MMERGE6" />
-					</div>
-					<div>
-						<label for="mce-PHONE">Phone Number </label>
-						<input type="text" name="PHONE" class="" value="" id="mce-PHONE" />
-					</div>
-					<div>
-						<label for="mce-MMERGE2">Message  <span class="asterisk">*</span></label>
-						<input type="text" value="" name="MMERGE2" class="required" id="mce-MMERGE2" required />
-					</div>
-
-					<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
 				</form>
 
 			</div >
---
+++ src/testing.js
@@ -0,0 +1,46 @@
+<div id="mc_embed_signup">
+    <form action="https://gmail.us21.list-manage.com/subscribe/post?u=ac1c083751d9fe609a58f1cfd&amp;id=9bda47a092&amp;f_id=000bcde1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
+        <div id="mc_embed_signup_scroll">
+        
+        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-MMERGE3">Your Name </label>
+	<input type="text" value="" name="MMERGE3" class="" id="mce-MMERGE3" />
+	<span id="mce-MMERGE3-HELPERTEXT" class="helper_text"></span>
+</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required />
+	<span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+</div>
+<div class="mc-field-group">
+	<label for="mce-FSUBJECT">Subject </label>
+	<input type="text" value="" name="FSUBJECT" class="" id="mce-FSUBJECT" />
+	<span id="mce-FSUBJECT-HELPERTEXT" class="helper_text"></span>
+</div>
+<div class="mc-field-group size1of2">
+	<label for="mce-PHONE">Phone Number </label>
+	<input type="text" name="PHONE" class="" value="" id="mce-PHONE" />
+	<span id="mce-PHONE-HELPERTEXT" class="helper_text"></span>
+</div>
+<div class="mc-field-group">
+	<label for="mce-MMERGE2">Message  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="MMERGE2" class="required" id="mce-MMERGE2" required />
+	<span id="mce-MMERGE2-HELPERTEXT" class="helper_text"></span>
+</div>
+	<div id="mce-responses" class="clear foot">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>   
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ac1c083751d9fe609a58f1cfd_9bda47a092" tabindex="-1" value="" /></div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
+                <p class="brandingLogo"><a href="http://eepurl.com/ih8RSr" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" /></a></p>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
