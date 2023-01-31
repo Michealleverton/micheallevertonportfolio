78e606488ee0541018e3a152839e2d589aba6c45
Micheal Leverton <levertonmicheal@gmail.com>
2023-01-31 13:25:11 -0500

    form email test

--- src/About.js
+++ src/About.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ProfilePic from './assets/profilepic2.png'
-import Imagemask from "./Imagemask"
+import Testform from './Testform'
+// import Imagemask from "./Imagemask"
 
 function About() {
   return (
     <section className='' id='about'>
 
       <div className='about-outside'>
-      <Imagemask />
+        {/* <Imagemask /> */}
 
         <div className='minfoholder'>
           <div className='profilecontainer textwhite mt-5'>
@@ -35,68 +36,7 @@ function About() {
           </div>
         </div>
 
-        <div className='formholder'>
-          <div className='contactcardholder'>
-
-            <div className='contactcard'>
-              <div className='hiremetitle'>
-                <p className='fs_2 emailtext' >Fullstackportfolio@gmail.com</p>
-              </div>
-
-              {/* <!-- Begin Mailchimp Signup Form --> */}
-              <div id="mc_embed_signup">
-                <form action="https://gmail.us21.list-manage.com/subscribe/post?u=ac1c083751d9fe609a58f1cfd&amp;id=9bda47a092&amp;f_id=000bcde1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
-                  <div id="mc_embed_signup_scroll">
-
-                    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-                    <div class="mc-field-group">
-                      <label for="mce-MMERGE3">Your Name </label>
-                      <input type="text" name="MMERGE3" class="" id="mce-MMERGE3" />
-                      <span id="mce-MMERGE3-HELPERTEXT" class="helper_text"></span>
-                    </div>
-                    <div class="mc-field-group">
-                      <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-                      </label>
-                      <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required />
-                      <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-                    </div>
-                    <div class="mc-field-group">
-                      <label for="mce-FSUBJECT">Subject </label>
-                      <input type="text" name="FSUBJECT" class="" id="mce-FSUBJECT" />
-                      <span id="mce-FSUBJECT-HELPERTEXT" class="helper_text"></span>
-                    </div>
-                    <div class="mc-field-group size1of2">
-                      <label for="mce-PHONE">Phone Number </label>
-                      <input type="text" name="PHONE" class="" id="mce-PHONE" />
-                      <span id="mce-PHONE-HELPERTEXT" class="helper_text"></span>
-                    </div>
-                    <div class="mc-field-group">
-                      <label for="mce-MMERGE2">Message  <span class="asterisk">*</span>
-                      </label>
-                      <textarea type="text" name="MMERGE2" col="30" row="30" class="required" id="mce-MMERGE2" required />
-                      <span id="mce-MMERGE2-HELPERTEXT" class="helper_text"></span>
-                    </div>
-                    <div id="mce-responses" class="clear foot">
-                      <div class="response" id="mce-error-response"></div>
-                      <div class="response" id="mce-success-response"></div>
-                    </div>
-                    <div className='hider' aria-hidden="true"><input type="text" name="b_ac1c083751d9fe609a58f1cfd_9bda47a092" tabindex="-1" /></div>
-                    <div class="optionalParent">
-                      <div class="clear foot">
-                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
-                        {/* <p class="brandingLogo"><a href="http://eepurl.com/ih8RSr" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" /></a></p> */}
-                      </div>
-                    </div>
-                  </div>
-                </form>
-              </div>
-
-              {/* <!--End mc_embed_signup--> */}
-
-            </div >
-
-          </div >
-        </div>
+        <Testform />
 
       </div>
 
---
+++ src/Testform.js
@@ -0,0 +1,64 @@
+import { React, useState } from 'react'
+
+function Testform() {
+
+    const [formState, setFormState] = useState({})
+
+    const changeHandler = (event) => {
+        setFormState({ ...formState, [event.target.name]: event.target.value })
+    }
+
+    const submitHandler = () => {
+        const config = {
+            SecureToken: "1d82c763-47c8-41d0-88a0-8eec88af8e56",
+            To: 'micheallevertonportfolio@gmail.com',
+            From: "email",
+            Subject: "subject",
+            Body: "name, phone, message",
+        }
+        if(window.Email) {
+            window.Email.send(config)
+        }
+    }
+
+    return (
+        <div className='contactcardholder'>
+            <div className='contactcard'>
+
+                <form onSubmit={submitHandler} className="form_contact">
+
+                    <div className="requireindicator"><span className="asterisk">*</span> indicates required</div>
+                    <div className="">
+                        <label>Your Name </label>
+                        <input onChange={changeHandler} type="text" name="Name" className="" id="name" />
+                    </div>
+                    <div className="">
+                        <label>Email Address  <span className="asterisk">*</span></label>
+                        <input onChange={changeHandler} type="email" name="EMAIL" className="" id="email" required />
+                    </div>
+                    <div className="">
+                        <label>Subject </label>
+                        <input onChange={changeHandler} type="text" name="SUBJECT" className="" id="subject" />
+                    </div>
+                    <div className="">
+                        <label>Phone Number </label>
+                        <input onChange={changeHandler} type="text" name="PHONE" className="" id="phone" />
+                    </div>
+                    <div className="">
+                        <label>Message  <span className="asterisk">*</span></label>
+                        <textarea onChange={changeHandler} name="Message" id="message" cols="30" rows="2"></textarea>
+                        {/* <input type="text" name="Message" className="" id="message" required /> */}
+                    </div>
+
+                    <div className="form_submit_btn">
+                        <input type="submit" name="Submit" id="submit" className="" />
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    )
+}
+
+export default Testform
